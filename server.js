a548be0bea0998ed3b32eea22d23e5859ffefd15
RestedWickedDev <jesusdperez@gmail.com>
2021-06-22 22:40:44 -0700

    node reddit clone project init

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require Libraries
+const express = require('express');
+
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+  });
+  
+
+// Start Server
+
+app.listen(3000, () => {
+    console.log('RedditJS listening on port localhost:3000!');
+  });
