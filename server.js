76226297bdb7c6bfefdd73a58697785e2663a2a6
RestedWickedDev <jesusdperez@gmail.com>
2021-06-22 23:45:27 -0700

    Users can submit and save posts

--- server.js
+++ server.js
@@ -11,9 +11,20 @@ const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set db
+require('./data/reddit-db');
+
+require('./controllers/posts')(app);
+
 // Routes
 app.get('/', (req, res) => {
     res.render('home');
+  });
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
   });
   
 
