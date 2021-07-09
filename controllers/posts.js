8044ade459e1a040b4481846a3c243208fc3c8ae
RestedWickedDev <jesusdperez@gmail.com>
2021-07-09 12:32:16 -0700

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,30 @@
 const Post = require('../models/post');
+const User = require('../models/user');
+const Comment = require('../models/comment');
 
 module.exports = (app) => {
     // CREATE
-    app.post('/posts/new', (req, res) => {  //
-        // INSTANTIATE INSTANCE OF POST MODEL
+    app.post('/posts/new', (req, res) => {
+        if (req.user) {
+        const userId = req.user._id;
         const post = new Post(req.body);
+        post.author = userId;
 
-        // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-        post.save(() => res.redirect('/'));
+        post
+            .save()
+            .then(() => User.findById(userId))
+            .then((user) => {
+            user.posts.unshift(post);
+            user.save();
+            // REDIRECT TO THE NEW POST
+            return res.redirect(`/posts/${post._id}`);
+            })
+            .catch((err) => {
+            console.log(err.message);
+            });
+        } else {
+        return res.status(401); // UNAUTHORIZED
+        }
     });
 
     // NEW
@@ -17,22 +34,24 @@ module.exports = (app) => {
         res.render('posts-new', {currentUser});
     });
 
-    // VIEW POSTS
+    // INDEX
     app.get('/', (req, res) => {
         const currentUser = req.user;
 
-        Post.find({}).lean()
-          .then((posts) => res.render('posts-index', { posts, currentUser }))
-          .catch((err) => {
+        const { user } = req;
+        console.log(req.cookies);
+        Post.find({}).lean().populate('author')
+        .then((posts) => res.render('posts-index', { posts, user ,currentUser }))
+        .catch((err) => {
             console.log(err.message);
-        })
-    })
+        });
+    });
 
-    // LOOK UP THE POST
+   // LOOK UP THE POST
     app.get('/posts/:id', (req, res) => {
         const currentUser = req.user;
-
-        Post.findById(req.params.id).lean().populate('comments')
+    
+        Post.findById(req.params.id).lean().populate('comments').populate('author')
         .then((post) => res.render('posts-show', { post, currentUser }))
         .catch((err) => {
             console.log(err.message);
@@ -42,8 +61,8 @@ module.exports = (app) => {
     // SUBREDDIT
     app.get('/n/:subreddit', (req, res) => {
         const currentUser = req.user;
-
-        Post.find({ subreddit: req.params.subreddit }).lean()
+        const { subreddit } = req.params;
+        Post.find({ subreddit }).lean().populate('author')
         .then((posts) => res.render('posts-index', { posts, currentUser }))
         .catch((err) => {
             console.log(err);
