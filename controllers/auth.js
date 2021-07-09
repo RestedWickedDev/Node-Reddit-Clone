c9861f47b7f9fdad423722d2851f3340691d742d
RestedWickedDev <jesusdperez@gmail.com>
2021-07-09 11:16:51 -0700

    Users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,4 +23,9 @@ module.exports = (app) => {
             return res.status(400).send({ err });
         });
     });
+    // LOGOUT
+    app.get('/logout', (req, res) => {
+        res.clearCookie('nToken');
+        return res.redirect('/');
+    });
 };
